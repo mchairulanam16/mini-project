06e95456398700edbd98bc526435714b3bb7edd8
mchairulanam16 <m.chairulanam16@gmail.com>
2024-03-09 14:15:13 +0700

    Resolve conflict

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/code', [App\Http\Controllers\CodeController::class, 'index'])->name('code');
 Route::get('/generate-code', [App\Http\Controllers\CodeController::class, 'store'])->name('generateCode');
 
+Route::get('/subject', [App\Http\Controllers\SubjectController::class, 'index'])->name('subject');
+Route::post('/subject-store', [App\Http\Controllers\SubjectController::class, 'store'])->name('subjectStore');
+Route::get('/subject/{$id}', [App\Http\Controllers\SubjectController::class, 'show'])->name('subjectDetail');
+Route::delete('/subject/{$id}', [App\Http\Controllers\SubjectController::class, 'destroy'])->name('subjectDelete');
+
 Route::get('/class', [App\Http\Controllers\KelasController::class, 'index'])->name('class');
 Route::get('/class-create', [App\Http\Controllers\KelasController::class, 'store'])->name('classCreate');
 Route::put('/class{$id}', [App\Http\Controllers\KelasController::class, 'update'])->name('classUpdate');
