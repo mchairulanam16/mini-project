943a1340b1af79ff74f08f5edd0f072feaf83297
mchairulanam16 <m.chairulanam16@gmail.com>
2024-03-09 07:45:29 +0700

    Adjustment

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Auth::routes([
-    'register' => false
-]);
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
