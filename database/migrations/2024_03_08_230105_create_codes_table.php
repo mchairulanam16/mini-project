2ddba963ab90b683040b0303661051ab4b94aa70
mchairulanam16 <m.chairulanam16@gmail.com>
2024-03-13 17:29:57 +0700

    Adjust DB

--- database/migrations/2024_03_08_230105_create_codes_table.php
+++ database/migrations/2024_03_08_230105_create_codes_table.php
@@ -17,7 +17,7 @@ class CreateCodesTable extends Migration
             $table->id();
             $table->string('name');
             $table->unsignedBigInteger('user_id');
-            $table->integer('id_used_by');
+            $table->integer('id_used_by')->nullable();
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
